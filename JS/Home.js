29e0df796f9930bc0016bf4bf87a27677f897cfb
Kenneth Zhu <yukuanz@umich.edu>
2023-11-29 15:53:00 -0500

    final submission

--- JS/Home.js
+++ JS/Home.js
@@ -5,12 +5,37 @@ document.addEventListener("DOMContentLoaded", function() {
     const totalWidth = imageWidths.reduce((acc, width) => acc + width, 0);
     const animationDuration = parseFloat(getComputedStyle(slider).animationDuration);
     const scrollTime = animationDuration * (totalWidth / slider.clientWidth);
+    
     function copyImages() {
         images.forEach((img, index) => {
             const clone = img.cloneNode(true);
             slider.insertBefore(clone, slider.children[index]); 
         });
     }
+    
     copyImages();
-    setInterval(copyImages, 10000); 
+    setInterval(copyImages, 10000);
+    
+    // Adding event listener to the button
+    const speedButton = document.getElementById('speedButton');
+    let isSlowingDown = false;
+    let isPaused = false;
+
+    speedButton.addEventListener('click', function() {
+        if (!isSlowingDown) {
+            if (!isPaused) {
+                slider.style.animationPlayState = 'paused';
+                isPaused = true;
+                speedButton.textContent = 'Play';
+            } else {
+                slider.style.animationPlayState = 'running';
+                isPaused = false;
+                speedButton.textContent = 'Pause';
+            }
+        } else {
+            slider.style.animation = 'scrollLeft 10000s linear infinite';
+            isSlowingDown = false;
+            speedButton.textContent = 'Pause';
+        }
+    });
 });
